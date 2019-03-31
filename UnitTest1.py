932a116a8656b5ca9c98c7f093de84c71b1ec557
Manikandan-Raj <maniksrview@gmail.com>
2019-03-30 19:02:09 -0700

    wrong

--- UnitTest1.py
+++ UnitTest1.py
@@ -7,7 +7,7 @@ class SimpleTest(unittest.TestCase):
     # Returns True or False.  
     def test(self):
         inst = Test1()
-        self.assertEquals(1,inst.method1())
+        self.assertEquals(10,inst.method1())
 
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_name="MyReport", add_timestamp=False))
